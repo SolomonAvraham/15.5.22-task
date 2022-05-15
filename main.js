a3717e5cb47396348555e18ede17c9e527ead911
SolomonAvraham <Selmon_a@hotmail.com>
2022-05-15 11:53:37 +0300

    update

---
+++ main.js
@@ -0,0 +1,17 @@
+var myArray = ["so", "yoni", "doron", "yaniv"];
+console.log(myArray);
+console.log(myArray[2]);
+
+var myArray = ["so", "yoni", "doron", "yaniv"];
+for(var i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+}
+
+
+var myArray = [100, 200, 300, 400, 500, 600, 700];
+console.log(myArray);
+console.log(myArray[3]);
+var myArray = [100, 200, 300, 400, 500, 600, 700];
+for (var i = 0; i < myArray.length; i++){
+    console.log(myArray[i]);
+}
